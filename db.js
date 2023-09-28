c44f9075a25e910852008baa8e5658358a4e3e1e
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-09-28 20:02:23 +0530

    new

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 import {MongoClient} from "mongodb";
 import Obj from "mongodb"
+import dotenv from "dotenv"
 
+// configure the envirenment.
+dotenv.config()
 
-const MongoURL="mongodb+srv://siddiq:sux1gtfN1dB8R9ye@project1.dee1vi6.mongodb.net/?retryWrites=true&w=majority";
+const secretkey=process.env.SECRETKEY;
+const MongoURL = `mongodb+srv://siddiq:${secretkey}@project1.dee1vi6.mongodb.net/?retryWrites=true&w=majority`;
+
 async function createConnection(){
     const client=new MongoClient(MongoURL);
     await client.connect();
